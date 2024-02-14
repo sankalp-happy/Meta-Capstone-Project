26a85499bbc31c9d40332aed30f97390e575b3da
Sankalp Shankar <sankalps.23.beai@acharya.ac.in>
2024-02-14 19:08:34 +0530

    Added components and OGP, semantic HTML

---
+++ src/comp/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Menu</a></li>
+                <li><a href="#contact">Reservations</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
